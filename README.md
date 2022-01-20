eba4d6070c8f1cd135aba71286b117eda6597eef
Heather Stevens <heatherveva@gmail.com>
2022-01-20 15:55:42 -0600

    hw5 almost done

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# hw5
+# Homework 5 Workday Scheduler
+
+## Description
+
+This code quiz was built using HTML, CSS, Javascript, JQuery, and Moment.
+
+## Screenshot
+
+![This is an image of the deployed workday schedule. The hours of the day are listed on the left. There is a space for the noting tasks in the middle and a blue save icon on the right.](assets/calendarscreenshot.png)
+
+## Links to Application
+
+- Here is a link to the repository: https://github.com/heatherveva/hw4.git
+- Here is the page: https://heatherveva.github.io/hw4/
--- javascript/script.js
+++ javascript/script.js
@@ -11,6 +11,23 @@ function saveTask() {
   localStorage.setItem(time, task);
 }
 
+function timeColorChange() {
+  const now = moment().hours();
+  $(".js-row").each(function () {
+    const row = $(this);
+    const rowHour = row.data("hour");
+    if (rowHour < now) {
+      $(this).addClass("past");
+    } else if (rowHour === now) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+}
+
+//loop through all the rows
+
 // event listeners
 // show current date
 $("#currentDay").text(moment().format("MMMM Do, YYYY"));
@@ -18,6 +35,15 @@ $("#currentDay").text(moment().format("MMMM Do, YYYY"));
 // retrieve tasks from local storage
 $("#hour9 .js-task").val(localStorage.getItem("hour9"));
 $("#hour10 .js-task").val(localStorage.getItem("hour10"));
+$("#hour11 .js-task").val(localStorage.getItem("hour11"));
+$("#hour12 .js-task").val(localStorage.getItem("hour12"));
+$("#hour13 .js-task").val(localStorage.getItem("hour13"));
+$("#hour14 .js-task").val(localStorage.getItem("hour14"));
+$("#hour15 .js-task").val(localStorage.getItem("hour15"));
+$("#hour16 .js-task").val(localStorage.getItem("hour16"));
+$("#hour17 .js-task").val(localStorage.getItem("hour17"));
+$("#hour18 .js-task").val(localStorage.getItem("hour18"));
 
 // user clicks save button
 $(".js-saveBtn").on("click", saveTask);
+timeColorChange();
